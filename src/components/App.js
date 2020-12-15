b560fc5a8b529b1407e3f072310f571721abc2c7
springlordpants <springlordpants@gmail.com>
2020-12-15 14:18:05 -0800

    Created MusicPlayer Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Global, css, jsx } from '@emotion/core'
+import MusicPlayer from './MusicPlayer'
 
 /**
  * @function App
@@ -7,6 +8,7 @@ import { Global, css, jsx } from '@emotion/core'
 const App = () => (
   <>
     <Global styles={GlobalCSS} />
+    <MusicPlayer />
   </>
 )
 
---
+++ src/components/MusicPlayer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { css, jsx } from '@emotion/core'
+
+const MusicPlayer = () => {
+  return (
+    <div className="MusicPlayer" css={CSS}>
+      MusicPlayer
+    </div>
+  )
+}
+
+const CSS = css``
+
+export default MusicPlayer
