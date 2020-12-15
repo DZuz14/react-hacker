ea45dde5a4f799c9dd4f6ad39ed43580de6984c1
springlordpants <springlordpants@gmail.com>
2020-12-15 14:11:13 -0800

    Set Defaults

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Global, css, jsx } from '@emotion/core'
-import reactLogo from '../img/react.png'
 
 /**
  * @function App
@@ -8,11 +7,6 @@ import reactLogo from '../img/react.png'
 const App = () => (
   <>
     <Global styles={GlobalCSS} />
-
-    {/* Delete me, the default .app styles in GlobalCSS, and the reactLogo img import */}
-    <img src={reactLogo} />
-    <h1>Hello Moto.</h1>
-    {/*  */}
   </>
 )
 
@@ -29,20 +23,6 @@ const GlobalCSS = css`
     height: 100%;
     width: 100%;
   }
-
-  // Just to position the react logo on initial startup. Delete this block.
-  .app {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    background: #282c34;
-
-    h1 {
-      color: white;
-    }
-  }
-  //
 
   a {
     text-decoration: none;
